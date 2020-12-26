4972c0f9cdc8e57dceffaddd3ccf8a5fd92f4885
Rubem Fridolino Christ Neto <rubemchrist@gmail.com>
2020-12-26 12:16:20 +0000

    table created

---
+++ chess.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void createTable()
+{
+    char row[9] = {'p','r','n','b','q','k','b','n','r'};
+    srand(time(0));
+
+    int random = rand()%2;
+
+    for (int i = 'A'; i <= 'H'; i++)
+    {
+        printf("   %c",i);
+    }
+    printf("\n");
+    if(random == 0)
+    {    
+        for (int l = 0; l < 8; l++)
+        {
+            printf("%d ",l+1);
+            for (int i = 0; i < 8; i++)
+            {
+                if(l+1 == 1)
+                {
+                    printf(" [%c]",row[i+1]);
+                }else if(l+1 == 2)
+                {
+                    printf(" [%c]",row[0]);
+                }else if(l+1 == 7)
+                {
+                    printf(" [%c]",toupper(row[0]));
+                }else if(l+1 == 8)
+                {
+                    printf(" [%c]",toupper(row[i+1]));
+                }else
+                {
+                    printf(" [ ]");
+                }                        
+            }
+            printf("\n");        
+        }
+    }else
+    {
+        for (int l = 0; l < 8; l++)
+        {
+            printf("%d ",l+1);
+            for (int i = 0; i < 8; i++)
+            {
+                if(l+1 == 1)
+                {
+                    printf(" [%c]",toupper(row[i+1]));
+                }else if(l+1 == 2)
+                {
+                    printf(" [%c]",toupper(row[0]));
+                }else if(l+1 == 7)
+                {
+                    printf(" [%c]",row[0]);
+                }else if(l+1 == 8)
+                {
+                    printf(" [%c]",row[i+1]);
+                }else
+                {
+                    printf(" [ ]");
+                }                        
+            }
+            printf("\n");        
+        }
+    }
+    
+}
--- main.c
+++ main.c
@@ -1,9 +1,63 @@
 #include <stdio.h>
-#include <chess.h>
-#include <fileio.h>
+#include <ctype.h>
+#include <string.h>
+#include "chess.h"
+#include "fileio.h"
+
+typedef struct 
+{
+    char v;
+    char h;
+    char index;    
+    //char pieces[6] = {'p','r','n','b','k','q'};    
+}piece;
+typedef struct
+{
+    char pi[6];// = {'p','r','n','b','k','q'};    
+}pieces;
+
+
 
 int main()
 {
+    char name1[10] = "";
+    char name2[10] = "";
+    createTable();
     return 0;
 }
+
+// typedef struct
+//     {
+//         int idade;
+//     }me;
+
+// int main()
+// {
+//     struct {
+//     char nome[60];
+//     int dia, mes, ano;
+//     } pessoas[4];
+
+    
+//     me eu[100];
+
+//     int i;
+//     for(i=0; i<4; i++) {
+//     printf("NOME: "); fgets(pessoas[i].nome,60,stdin);
+//     printf("DATA NASCIMENTO:\n");
+//     printf("\tDIA: "); scanf("%d", &pessoas[i].dia);
+//     printf("\tMES: "); scanf("%d", &pessoas[i].mes);
+//     printf("\tANO: "); scanf("%d", &pessoas[i].ano);
+//     //fflush(stdin);
+//     while( getchar() != '\n');
+//     }
+//     printf("LISTA:\n");
+//     for(i=0; i<4; i++) {
+//     printf("[%d] %s ", i, pessoas[i].nome);
+//     printf("%d-%d-%d\n",pessoas[i].dia,pessoas[i].mes,pessoas[i].ano);
+//     }
+
+
+//     return 0;
+// }
 
