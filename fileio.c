fcc3271909c779e24f57e497bec2cfc326753538
Alexpicarvalho <59317333+Alexpicarvalho@users.noreply.github.com>
2020-12-30 16:54:51 +0000

    main update

--- fileio.c
+++ fileio.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+/* 
 void createFile() {
 	FILE* game_state;
 
@@ -19,4 +19,4 @@ void createFile() {
 void writeInFile() {
 
 
-}
+} */
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "chess.h"
 #include "fileio.h"
+#include "Players.c"
 
 int main()
 {    
+    createPlayers();
     createTable();
     return 0;
 }
