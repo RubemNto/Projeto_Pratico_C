ab0922b8635605ab4fd3c84f7764e1c0e35f794f
Alexpicarvalho <59317333+Alexpicarvalho@users.noreply.github.com>
2020-12-31 16:00:09 +0000

    Alguns updates no código e pseudocodigo para os move input dos players

--- Players.c
+++ Players.c
@@ -7,13 +7,16 @@
 #include "fileio.h"
 
 
-//Individual Player Struct - Defines PlayerName
+//------------- Individual Player Struct - Defines PlayerName  ------------------//
   typedef struct
 {
     char nickname[12];
 }player;
 player players[2];     // Creates an array where the 2 players in the game will be stored and manipulated
+
   
+  //------------------------  FUNCTION TO SET NICKNAMES   ---------------------------//
+
 void createPlayers(){
                             //CYCLE TO REQUEST FOR PLAYER'S NICKNAME AND STORE IT IN """THE PLAYERSLIST"""
      do                     //GRANTS PLAYERS DON'T PICK THE SAME NICKNAME
@@ -21,7 +24,7 @@ void createPlayers(){
         for (int i = 0; i < 2; i++)
         {
             printf("Player %d's Nickname ",(i + 1));
-            scanf("%s",&players[i].nickname);     
+            gets(players[i].nickname);     
         }
         if (!strcmp(players[0].nickname , players[1].nickname))
         {
@@ -30,7 +33,11 @@ void createPlayers(){
               
     } while (!strcmp(players[0].nickname , players[1].nickname));      
 }
+
+
 //--------------------   FUNCTION TO INFORM THE PLAYERS OF WHO PLAYS FIRST/ IS WHITE  ------------------//
+
+
 void setWhite(player players[2]){
 
     srand(time(NULL));
@@ -47,13 +54,97 @@ void setWhite(player players[2]){
             players[i] = temp[i];
         }   
         printf(players[0].nickname);
-        printf(" is White and will start the game \n \n");       
+        printf(" is White and will start the game! \n \n");
+        printf(players[1].nickname);
+        printf(" is Black and will go second! \n \n");       
     }
     else{ //ELSE ALL REMAINS THE SAME AND INITIAL PLAYER[0] STARTS
         printf(players[0].nickname);
-        printf(" is White and will start the game \n \n"); 
+        printf(" is Whites and will start the game! \n \n");
+        printf(players[1].nickname);
+        printf(" is Blacks and will go second! \n \n");  
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                         GAMEPLAY                                                               //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void gameCourse(){
+
+    do
+    {
+        makePlay(players[0]);
+        tableUpdate();
+        makePlay(players[1]);
+        tableUpdate();
+
+         // -----OR----- //
+        makePlayWhite();
+        tableUpdate();
+        makePlayBlack();
+        tableUpdate();
+
+    } while (!gameEnded());
+    
+}
+
+void makePlay(player player){
+
+    //POINT A -----> ALLOW PLAYER TO CHOOSE AN X AND Y COORDENATE TO MOVE A PIECE 
+
+    //POINT B -----> SHOW PLAYER THE POSSIBLE POSITION WHERE THE PIECE CAN GO  
+
+    //POINT C -----> TAKE PLAYER INPUT ABOUT WHERE TO MOVE THE PIECE OR ALLOW HIM TO GO BACK TO POINT A
+
+    //POINT D -----> ASK FOR COMFIRMATION ABOUT THE MOVE OR GO BACK TO POINT C
+
+    //POINT E -----> FINALIZE MOVE AND UPDATE TABLE (INSIDE OR OUTSIDE THIS FUNCTION TBD) AND LEAVE FUNCTION
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //_________________TEST____________________//
