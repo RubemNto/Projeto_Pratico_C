66425e906a66822f6794adc3aa4a41c3955dd6c2
Alexpicarvalho <59317333+Alexpicarvalho@users.noreply.github.com>
2020-12-31 15:23:27 +0000

    Comentário adicionado

--- Players.c
+++ Players.c
@@ -7,15 +7,60 @@
 #include "fileio.h"
 
 
-//char player [2] [12];
+//Individual Player Struct - Defines PlayerName
   typedef struct
 {
-    //Individual Player Struct - Defines PlayerName
     char nickname[12];
 }player;
-player players[2];
+player players[2];     // Creates an array where the 2 players in the game will be stored and manipulated
   
+void createPlayers(){
+                            //CYCLE TO REQUEST FOR PLAYER'S NICKNAME AND STORE IT IN """THE PLAYERSLIST"""
+     do                     //GRANTS PLAYERS DON'T PICK THE SAME NICKNAME
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Player %d's Nickname ",(i + 1));
+            scanf("%s",&players[i].nickname);     
+        }
+        if (!strcmp(players[0].nickname , players[1].nickname))
+        {
+            printf("Players Nicknames can't be the same. Please choose new ones! \n");
+        }
+              
+    } while (!strcmp(players[0].nickname , players[1].nickname));      
+}
+//--------------------   FUNCTION TO INFORM THE PLAYERS OF WHO PLAYS FIRST/ IS WHITE  ------------------//
+void setWhite(player players[2]){
 
+    srand(time(NULL));
+    int random = rand();       //Generates random value -> If pair Player[0] stays player 0 ----> ELSE Player[0] becomes player[1]
+    player temp[2];            //USER DOESN'T SEE THIS CHANGE HAPPENING, IT JUST ORGANIZES PLAYER ORDER
+
+    if(random % 2 == 1){
+       
+       // player[0] becomes player[1] and vice versa -----> player[0] always starts, but which one it is is random
+        temp[0] = players[1];
+        temp[1] = players[0];
+        for (int i = 0; i < 2; i++)
+        {
+            players[i] = temp[i];
+        }   
+        printf(players[0].nickname);
+        printf(" is White and will start the game \n \n");       
+    }
+    else{ //ELSE ALL REMAINS THE SAME AND INITIAL PLAYER[0] STARTS
+        printf(players[0].nickname);
+        printf(" is White and will start the game \n \n"); 
+    }
+}
+
+
+//_________________TEST____________________//
+
+
+
+//char player [2] [12];
 /* void createPlayers(){
   
      do
@@ -33,24 +78,7 @@ player players[2];
     } while (!strcmp(player[0] , player[1]));       
 } */
 
- void createPlayers(){
-  
-     do
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            printf("Player %d's Nickname ",(i + 1));
-            scanf("%s",&players[i].nickname);     
-        }
-        if (!strcmp(players[0].nickname , players[1].nickname))
-        {
-            printf("Players Nicknames can't be the same. Please choose new ones! \n");
-        }
-              
-    } while (!strcmp(players[0].nickname , players[1].nickname)); 
-
-       
-} 
+ 
 
 /* void setWhite(char player[] ){
 
@@ -75,26 +103,4 @@ player players[2];
     
 } */
 
- void setWhite(player players[2]){
-
-    srand(time(NULL));
-    int random = rand();
-    player temp[2];
-
-    if(random % 2 == 1){
-       
-       // player[0] becomes player[1] and vice versa -----> player[0] always starts, but which one it is is random
-        temp[0] = players[1];
-        temp[1] = players[0];
-        for (int i = 0; i < 2; i++)
-        {
-            players[i] = temp[i];
-        }   
-        printf(players[0].nickname);
-        printf(" is White and will start the game \n \n");       
-    }
-    else{ //ELSE ALL REMAINS THE SAME AND INITIAL PLAYER[0] STARTS
-        printf(players[0].nickname);
-        printf(" is White and will start the game \n \n"); 
-    }
-} 
+  
